44f0c2e67d8b6238b7d385260335c0a10f4a63ad
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-08-22 10:22:29 +0200

    mi segundocommit

--- numero.py
+++ numero.py
@@ -1,8 +1,8 @@
-cant=input("digite la cantidad de numeros:")
+cant=int(input("digite la cantidad de numeros:"))
 suma=0
 promedio=0
 for i in range(1, cant+1):
-    num=int(input("digite el numero"))
+    num=int(input("digite el numero:"))
     suma=suma+num
     print("suma:",suma) 
 
---
+++ secuencia.py
@@ -0,0 +1,4 @@
+n=int(input("digite la cantidad de numeros:"))
+for i in range(1,n):
+     print("numeros:",i)
+    
